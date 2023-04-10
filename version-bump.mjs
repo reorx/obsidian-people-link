d816f421078e2b82f1053dd8f57112a5eb4b51f8
Reorx <novoreorx@gmail.com>
2023-04-10 15:14:47 +0800

    chore: improve version-bump script

--- version-bump.mjs
+++ version-bump.mjs
@@ -1,14 +1,32 @@
-import { readFileSync, writeFileSync } from "fs";
+import { readFileSync, writeFileSync } from 'fs';
+import readline from 'readline';
 
-const targetVersion = process.env.npm_package_version;
 
-// read minAppVersion from manifest.json and bump version to target version
-let manifest = JSON.parse(readFileSync("manifest.json", "utf8"));
-const { minAppVersion } = manifest;
-manifest.version = targetVersion;
-writeFileSync("manifest.json", JSON.stringify(manifest, null, "\t"));
+const currentVersion = process.env.npm_package_version;
 
-// update versions.json with target version and minAppVersion from manifest.json
-let versions = JSON.parse(readFileSync("versions.json", "utf8"));
-versions[targetVersion] = minAppVersion;
-writeFileSync("versions.json", JSON.stringify(versions, null, "\t"));
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let newVersion
+rl.question(`Input new version (current is ${currentVersion}): `, (value) => {
+	newVersion = value
+	console.log('inputed', value, newVersion)
+	rl.close();
+
+	// update version in package.json
+	const packagejson = readFileSync("package.json", "utf8")
+	writeFileSync("package.json", packagejson.replace(`"version": "${currentVersion}"`, `"version": "${newVersion}"`));
+
+	// read minAppVersion from manifest.json and bump version to target version
+	let manifest = JSON.parse(readFileSync("manifest.json", "utf8"));
+	const { minAppVersion } = manifest;
+	manifest.version = newVersion;
+	writeFileSync("manifest.json", JSON.stringify(manifest, null, "\t"));
+
+	// update versions.json with target version and minAppVersion from manifest.json
+	let versions = JSON.parse(readFileSync("versions.json", "utf8"));
+	versions[newVersion] = minAppVersion;
+	writeFileSync("versions.json", JSON.stringify(versions, null, "\t"));
+});
